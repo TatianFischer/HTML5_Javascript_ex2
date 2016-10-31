5d462a8b83916c608af36898894d815c52273afb
tatianaflo <tatianaflo@hotmail.fr>
2016-10-31 12:42:50 +0100

    création des fonctions valider et reset; tableau des pays

---
+++ code.js
@@ -0,0 +1,34 @@
+// Jeu des pays
+
+//Ecriture du titre
+document.getElementById("titre").innerHTML = '<p>Jeu des pays-stan</p>';
+
+//Tableau des pays
+var tableauPays = ['', 'Azerbaïdjan', 'Turkménistan', 'Ouzbékistan', 'Afghanistan', 'Pakistan', 'Tadjikistan', 'Kirghizistan'];
+
+// Fonction Valider
+function fonctionValider(){
+	//boucle sur les listes
+	for (var i=1; i<=7; i++) {
+		// récupération de l'index, puis de la valeur choisie
+		var indexChoisi = document.forms["listes"].elements["liste"+i].selectedIndex;
+		var paysChoisi = document.forms["listes"].elements["liste"+i].options[indexChoisi].value;
+		//Teste si la liste i affiche le bon pays
+		if(paysChoisi == tableauPays[i]){
+			// Changement de style de l'élément numéro i
+			document.getElementById("numero"+i).style.backgroundColor='#0066CC';
+		}else{
+			document.getElementById("numero"+i).style.backgroundColor='#FFFFFF';
+		}
+	}
+}
+
+// Fonction Reset
+function fonctionReset(){
+	//boucle sur les listes
+	for(var i=1; i<=7; i++) {
+		// affichage première valeur
+		document.forms["listes"].elements["liste"+i].selectedIndex = 0;
+		document.getElementById("numero"+i).style.backgroundColor='#FFF';
+	}
+}
